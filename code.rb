3c4199665bd702ed15c27fc1c1493d3b51870c94
cherifb16 <cherifbobbyray@gmail.com>
2021-11-09 12:47:33 +0200

    ruby sample codes

---
+++ code.rb
@@ -0,0 +1,61 @@
+# concaténation
+
+    # car =  "Ferrari"  
+    # puts "my favorite car is" + car
+    # puts "One of the luxury cars is the" + car
+
+# Expansion de l'expression
+    # car =  "Ferrari"
+    # puts "my favorite car is #{car}"
+    # puts "One of the luxury cars is the #{car}"
+
+# compter les caractères dans un mot d'un utilisateur
+
+    # puts "enter any word"
+    # word = gets.chomp
+    # word.length
+    # puts " the length of the entered word/sentence is:  #{word.length}"
+
+
+# Application blog
+
+    # puts "Veuillez sélectionner l'action ci-dessous\n"
+    # puts "1: Créer un blog"
+    # puts "2: Quitter l'application"
+    # number = gets.to_i
+    # if number == 1
+    #   puts "1 : Créer un blog\n\n"
+    #   puts "Entrez le titre du blog"
+    #   blog_title = gets
+    #   puts "Entrez le contenu du blog"
+    #   blog_content = gets
+    #   puts "Titre et corps du texte saisis\n\n"
+    #   puts "Title：#{blog_title}"
+    #   puts "Contenu：#{blog_content}"
+    # elsif number == 2
+    #     puts "2: Quitter l'application" 
+    # else
+    #   puts "Veuillez entrer un nombre de 1 à 2"
+    # end
+
+# Déclaration "Unless".
+
+    # age = 22
+    # unless age < 20
+    #  puts "I'm not a minor"
+    # end
+# Déclaration case
+  # age = 20
+  # case age
+  # when 10
+  #   puts "I'm still a kid"
+  # when 20 
+  #   puts "Became an adult"
+  # when 60
+  #   puts "Retirement age"
+  # else
+  #   puts ""
+  # end
+
+
+
